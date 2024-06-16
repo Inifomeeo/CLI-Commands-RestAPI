5056d8f386d58acbd559838430fd15f072d159cb
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-06-16 15:24:27 +0100

    Add GET HTTP method

---
+++ CommandApi/Controllers/CommandsController.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using CommandApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using CommandApi.Data;
+
+namespace CommandApi.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {   
+        private readonly MockCommandApiRepo _repository = new MockCommandApiRepo();
+        // GET: api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        // GET: api/commands
+        // <snippet_GetByID>
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(long id)
+        {
+            var commandItem = _repository.GetCommandByID(id);
+
+            return Ok(commandItem);
+        }
+
+    }
+}
--- CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
+++ CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad518bb1383e213019ddf4eee0c168b695c9688f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff72c09582f734e7dd8b97ae43e0de04146aa72")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
