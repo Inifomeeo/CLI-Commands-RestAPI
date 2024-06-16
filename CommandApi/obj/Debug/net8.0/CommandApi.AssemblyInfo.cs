014aa4f08149fe75878076d95d865c79933abf62
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-06-16 13:55:10 +0100

    Delete default model and controller

--- CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
+++ CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c5883c5dea43a0872db602db4f29432ea44713")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9975d67af1207da861720f0732919435c0182722")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
