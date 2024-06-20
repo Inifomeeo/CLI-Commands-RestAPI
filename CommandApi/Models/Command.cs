c6730f6de0f1ff1f5d6b32aa18a1a82480a040e6
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-06-20 03:38:12 +0100

    Set Command model elements to "required"

--- CommandApi/Models/Command.cs
+++ CommandApi/Models/Command.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CommandApi.Models;
 
 public class Command
 {
     public long Id { get; set; }
+
+    [Required]
     public string? HowTo { get; set; }
+
+    [Required]
     public string? Line { get; set; }
+
+    [Required]
     public string? Platform { get; set; }
 }
