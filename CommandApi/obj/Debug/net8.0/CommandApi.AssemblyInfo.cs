32e9fa5fe7bee9c7c4435094300622a2ff632c6b
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-06-24 08:44:20 +0100

    Remove mock repo

--- CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
+++ CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2827ee3efe88a79857f6eb894e84b42711957eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e4b6fc09aac753f68dc7942d63930802c80cf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
