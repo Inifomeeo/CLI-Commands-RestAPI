fba8f0509782f09783b3233ac530d18514aa2488
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-06-24 07:20:47 +0100

    Create DTO for updating commands

---
+++ CommandApi/DTOs/CommandUpdateDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandApi.Dtos
+{
+    public class CommandUpdateDto
+    {
+        [Required]
+        public string? HowTo { get; set; }
+
+        [Required]
+        public string? Line { get; set; }
+
+        [Required]
+        public string? Platform { get; set; }
+    }
+}
