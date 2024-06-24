d78f750c87b8df131fe21125b4fd23b92059d9dd
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-06-24 04:25:14 +0100

    Test project

--- CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
+++ CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b52d09ef8d69ea9faaccd190e026ac61df2c1eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7cf27671b6f2e256a0b5659a26d3a8004b4220a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
