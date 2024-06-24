0a4e069a4ff40b960b062598100b6dbdbb71caeb
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-06-24 03:57:37 +0100

    Create DTO for creating commands

---
+++ CommandApi/DTOs/CommandCreateDto.cs
@@ -0,0 +1,11 @@
+namespace CommandApi.Dtos
+{
+    public class CommandCreateDto
+    {
+        public string? HowTo { get; set; }
+
+        public string? Line { get; set; }
+
+        public string? Platform { get; set; }
+    }
+}
--- CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
+++ CommandApi/obj/Debug/net8.0/CommandApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4650b2333147c3ab04d5ae5bd637f01ffd7b8ead")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ec55fa72842a8d84e76ba11f02fba94145f20f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
