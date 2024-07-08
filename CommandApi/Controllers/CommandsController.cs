ff9d5d2efc7b079daae26d4bdb5bb1127bda960d
Inifomeeo <78764279+Inifomeeo@users.noreply.github.com>
2024-07-08 05:18:27 +0100

    Fixed typo

--- CommandApi/Controllers/CommandsController.cs
+++ CommandApi/Controllers/CommandsController.cs
@@ -115,7 +115,7 @@ namespace CommandApi.Controllers
         }
         // <snippet_Update>
 
-        // DELETE: api/TodoItems/{id}
+        // DELETE: api/commands/{id}
         [HttpDelete("{id}")]
         public ActionResult DeleteCommand(long id)
         {
